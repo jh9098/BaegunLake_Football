b08e9516b93c0ff5e8e0912cad5219804c574da4
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-07-21 21:18:13 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { imagetools } from "vite-imagetools";   // ★ 추가
 
 export default defineConfig({
   plugins: [
     react(),
+    imagetools(),                                // ★ 추가
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
